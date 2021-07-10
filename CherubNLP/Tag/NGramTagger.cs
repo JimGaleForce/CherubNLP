682f8816d29e05eda56cfdb59bf80c9759594679
Jim Gale <jimgaleforce@gmail.com>
2021-07-09 18:58:31 -0700

    parallel-friendly speed ups in Tagging, WordNet

--- CherubNLP/Tag/NGramTagger.cs
+++ CherubNLP/Tag/NGramTagger.cs
@@ -16,48 +16,67 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using CherubNLP.Corpus;
+using CherubNLP.Tokenize;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using CherubNLP.Corpus;
-using CherubNLP.Tokenize;
 
 namespace CherubNLP.Tag
 {
     /// <summary>
-    /// N-Gramm taggers are based on a simple statistical algorithm: 
-    /// for each token, assign the tag that is most likely for that particular token.
+    /// N-Gramm taggers are based on a simple statistical algorithm:  for each token, assign the tag that is most likely
+    /// for that particular token.
     /// </summary>
     public class NGramTagger : ITagger
     {
         private List<NGramFreq> _contextMapping { get; set; }
+
+        private Dictionary<string, string> _tags1 { get; set; }
 
         public void Tag(Sentence sentence, TagOptions options)
         {
             // need training to generate model
             if(_contextMapping == null)
             {
-                var corpus = new CoNLLReader().Read(new ReaderOptions
-                {
-                    DataDir = Path.Combine(options.CorpusDir, "CoNLL"),
-                    FileName = "conll2000_chunking_train.txt"
-                });
+                var corpus = new CoNLLReader().Read(
+                    new ReaderOptions
+                    {
+                        DataDir = Path.Combine(options.CorpusDir, "CoNLL"),
+                        FileName = "conll2000_chunking_train.txt"
+                    });
 
                 Train(corpus, options);
             }
 
             Fill(sentence, options);
 
-            for (int pos = options.NGram - 1; pos < sentence.Words.Count; pos++)
+            for(int pos = options.NGram - 1; pos < sentence.Words.Count; pos++)
             {
-                sentence.Words[pos].Pos = _contextMapping.FirstOrDefault(x => x.Context == GetContext(pos, sentence.Words, options))?.Tag;
-
-                // set default tag
-                if(sentence.Words[pos].Pos == null)
+                if(options.NGram == 1)
                 {
-                    sentence.Words[pos].Pos = options.Tag;
+                    var text = sentence.Words[pos].Text;
+                    if(_tags1.ContainsKey(text))
+                    {
+                        sentence.Words[pos].Pos = _tags1[text];
+                    }
+                    else
+                    {
+                        sentence.Words[pos].Pos = options.Tag;
+                    }
+                }
+                else
+                {
+                    sentence.Words[pos].Pos = _contextMapping.FirstOrDefault(
+                        x => x.Context == GetContext(pos, sentence.Words, options))?.Tag;
+
+                    // set default tag
+                    if(sentence.Words[pos].Pos == null)
+                    {
+                        sentence.Words[pos].Pos = options.Tag;
+                    }
                 }
             }
 
@@ -71,17 +90,20 @@ namespace CherubNLP.Tag
         {
             var cache = new List<NGramFreq>();
 
-            for (int idx = 0; idx < sentences.Count; idx++)
+            for(int idx = 0; idx < sentences.Count; idx++)
             {
                 var sent = sentences[idx];
 
                 Fill(sent, options);
 
-                for (int pos = options.NGram - 1; pos < sent.Words.Count; pos++)
+                for(int pos = options.NGram - 1; pos < sent.Words.Count; pos++)
                 {
                     var freq = new NGramFreq
                     {
-                        Context = GetContext(pos, sent.Words, options),
+                        Context =
+                            (options.NGram == 2
+                                ? sent.Words[pos - 1].Pos + " " + sent.Words[pos].Text
+                                : GetContext(pos, sent.Words, options)),
                         Tag = sent.Words[pos].Pos,
                         Count = 1
                     };
@@ -91,35 +113,53 @@ namespace CherubNLP.Tag
             }
 
             _contextMapping = (from c in cache
-                               group c by new { c.Context, c.Tag } into g
-                               select new NGramFreq
-                               {
-                                   Context = g.Key.Context,
-                                   Tag = g.Key.Tag,
-                                   Count = g.Count()
-                               }).OrderByDescending(x => x.Count)
-                               .ToList();
+                group c by new { c.Context, c.Tag } into g
+                select new NGramFreq { Context = g.Key.Context, Tag = g.Key.Tag, Count = g.Count() }).OrderByDescending(
+                x => x.Count)
+                .ToList();
+
+            if(options.NGram == 1)
+            {
+                _tags1 = new Dictionary<string, string>();
+                foreach(var cm in _contextMapping)
+                {
+                    if(!_tags1.ContainsKey(cm.Context))
+                    {
+                        _tags1.Add(cm.Context, cm.Tag);
+                    }
+                }
+            }
         }
 
         private string GetContext(int pos, List<Token> words, TagOptions options)
         {
-            string context = words[pos].Text;
-            for (int ngram = options.NGram - 1; ngram > 0; ngram--)
+            if(options.NGram == 2)
             {
-                context = words[pos - ngram].Pos + " " + context;
+                return words[pos - 1].Pos + " " + words[pos].Text;
+            }
+            else if(options.NGram == 3)
+            {
+                return words[pos - 2].Pos + " " + words[pos - 1].Pos + " " + words[pos].Text;
             }
 
-            return context;
+            StringBuilder context = new StringBuilder();
+            context.Append(words[pos].Text);
+            for(int ngram = options.NGram - 1; ngram > 0; ngram--)
+            {
+                context.Insert(0, words[pos - ngram].Pos + " ");
+            }
+
+            return context.ToString();
         }
 
         private void Fill(Sentence sent, TagOptions options)
         {
-            for (int ngram = 1; ngram < options.NGram; ngram++)
+            for(int ngram = 1; ngram < options.NGram; ngram++)
             {
                 sent.Words.Insert(0, new Token { Text = "NIL", Pos = options.Tag, Start = (ngram - 1) * 3 });
             }
         }
-        
+
         private class NGramFreq
         {
             /// <summary>
